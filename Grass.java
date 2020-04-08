73585502efa2d890451ba9606618672f700410fe
p-lawlor <parkerlawlor2014@gmail.com>
2020-04-08 13:14:18 -0700

    2nd commit

--- Grass.java
+++ Grass.java
@@ -12,6 +12,7 @@ public class Grass extends Plant {
 		int newX = (int)(Math.random()*20);
 		int newY = (int)(Math.random()*20); 
 		System.out.println("hopefully this will work");
+		System.out.println("hello");
 		
 		myWorld.getCreatureList().add(new Grass(new Location(newX,newY), myWorld));
 	}
